c7cb0b4b3459a3851920b005d934e81c835eaaa3
rere <regis-graptin@sfr.fr>
2023-01-31 20:14:47 +0100

    feat: define variables for flowerArea

--- HummingBirds/Assets/Hummingbird/Scripts/FlowerArea.cs
+++ HummingBirds/Assets/Hummingbird/Scripts/FlowerArea.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/// <summary>
+/// Manages a collection of flower plants and attached flowers.
+/// </summary>
 public class FlowerArea : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Diameter area to indicate the relative distance between the agent and flowers.
+    public const float AreaDiameter = 20f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // The list of all flower plants in this flower area.
+    private List<GameObject> flowerPlants;
+
+    // A lookup dictionary for looking up a flower from a nectar collider
+    private Dictionary<Collider, Flower> nectarFlowerDictionary;
+
+    /// <summary>
+    /// The list of all flowers in the flower area.
+    /// </summary>
+    public List<Flower> Flowers { get; private set; }
+
+
 }
