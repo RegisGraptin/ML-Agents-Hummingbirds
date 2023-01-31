8a149107c83aa31d46e407d4aede9049c8fd0c5e
rere <regis-graptin@sfr.fr>
2023-01-31 20:34:42 +0100

    feat: add functions for the flowerArea class

--- HummingBirds/Assets/Hummingbird/Scripts/FlowerArea.cs
+++ HummingBirds/Assets/Hummingbird/Scripts/FlowerArea.cs
@@ -22,5 +22,94 @@ public class FlowerArea : MonoBehaviour
     /// </summary>
     public List<Flower> Flowers { get; private set; }
 
+    /// <summary>
+    /// Resert the flowers and flower plants.
+    /// </summary>
+    public void ResetFlowers()
+    {
+        // Rotate each flower in the flower around the Y axis and subtly around X and Z
+        foreach (GameObject flowerPlant in flowerPlants)
+        {
+            float xRotation = UnityEngine.Random.Range(-5f, 5f);
+            float yRotation = UnityEngine.Random.Range(-180f, 180f);
+            float zRotation = UnityEngine.Random.Range(-5f, 5f);
+
+            flowerPlant.transform.localRotation = Quaternion.Euler(xRotation, yRotation, zRotation);
+        }
+
+        // Reset each flower
+        foreach (Flower flower in Flowers)
+        {
+            flower.ResetFlower();
+        }
+    }
+
+    /// <summary>
+    /// Get the <see cref="Flower"/> that a nectar collider belongs to
+    /// </summary>
+    /// <param name="collider">The nectar collider</param>
+    /// <returns>The matrching flower</returns>
+    public Flower GetFlowerFromNectar(Collider collider)
+    {
+        return nectarFlowerDictionary[collider];
+    }
+
+
+    /// <summary>
+    /// Called when the area wakes up.
+    /// </summary>
+    private void Awake()
+    {
+        flowerPlants = new List<GameObject>();
+        nectarFlowerDictionary = new Dictionary<Collider, Flower>();
+        Flowers = new List<Flower>();
+    }
+
+    /// <summary>
+    /// Called when the game starts.
+    /// </summary>
+    private void Start()
+    {
+        // Find all flowers that are children of this GameObject/Transform
+        FindChildFlowers(transform);
+    }
+
+    /// <summary>
+    /// Recusively finds all flower that are children of a parent GameObject/Transform
+    /// </summary>
+    /// <param name="parent">The parent of the children to check</param>
+    private void FindChildFlowers(Transform parent)
+    {
+        for (int i = 0; i < parent.childCount; i++)
+        {
+            Transform child = parent.GetChild(i);
+
+            if (child.CompareTag("flower_plant"))
+            {
+                // Find a flower plant, add it to the flowerPlants list
+                flowerPlants.Add(child.gameObject);
+
+                // Look for flowers within the flower plant
+                FindChildFlowers(child);
+            }
+            else 
+            {
+                // Not a flower plant, look for a Flower component
+                Flower flower = child.GetComponent<Flower>();
+                if (flower != null) 
+                {
+                    // Add the flower 
+                    Flowers.Add(flower);
+
+                    // Add the nectar collider
+                    nectarFlowerDictionary.Add(flower.nectarCollider, flower);
+                }
+                else 
+                {
+                    FindChildFlowers(child);
+                }
+            }
+        }
+    }
 
 }
